934e6ff65b3f1c3b3ce89df5f53398e4c0662062
IvanRudik <admin@Admins-MBP.fritz.box>
2024-07-04 13:44:49 +0200

    New CategoryRepository for Category

---
+++ src/main/java/com/ivanrudik/store/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.ivanrudik.store.repository;
+
+import com.ivanrudik.store.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
