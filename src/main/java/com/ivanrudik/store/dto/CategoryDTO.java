4a2bfafd0de03f9ae077012575f883ed1547038e
IvanRudik <admin@Admins-MBP.fritz.box>
2024-07-04 16:51:25 +0200

    DTO objects for entities

---
+++ src/main/java/com/ivanrudik/store/dto/CategoryDTO.java
@@ -0,0 +1,20 @@
+package com.ivanrudik.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private List<ProductDTO> products = new ArrayList<>();
+}
---
+++ src/main/java/com/ivanrudik/store/mapper/CategoryMapper.java
@@ -0,0 +1,12 @@
+package com.ivanrudik.store.mapper;
+
+import com.ivanrudik.store.dto.CategoryDTO;
+import com.ivanrudik.store.entity.Category;
+
+public class CategoryMapper {
+
+    public static CategoryDTO mapToCategoryDTO(Category category) {
+        
+        return null;
+    }
+}
