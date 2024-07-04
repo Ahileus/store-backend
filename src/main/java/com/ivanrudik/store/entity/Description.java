44e136c944f39b2d6091fa92d95cc1bdf4d43e39
IvanRudik <admin@Admins-MBP.fritz.box>
2024-07-04 13:02:27 +0200

    Fields for Entities

--- src/main/java/com/ivanrudik/store/entity/Description.java
+++ src/main/java/com/ivanrudik/store/entity/Description.java
@@ -1,4 +1,17 @@
 package com.ivanrudik.store.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Description {
+    private Long id;
+    private String manufacturer;
+    private String manufactureDate;
+    private String details;
 }
--- src/main/java/com/ivanrudik/store/entity/Product.java
+++ src/main/java/com/ivanrudik/store/entity/Product.java
@@ -1,4 +1,19 @@
 package com.ivanrudik.store.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer stock;
+    private Description description;
+
 }
