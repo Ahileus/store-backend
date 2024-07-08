bfad5a7970a527e8dcfc855a6b57db72daa73230
IvanRudik <admin@Admins-MBP.fritz.box>
2024-07-08 16:51:54 +0200

    added swagger

--- src/main/java/com/ivanrudik/store/controller/CategoryController.java
+++ src/main/java/com/ivanrudik/store/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.ivanrudik.store.controller;
 
 import com.ivanrudik.store.dto.CategoryDTO;
 import com.ivanrudik.store.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Category controller")
 @RestController
 @RequestMapping("/api/categories")
 @AllArgsConstructor
@@ -16,18 +19,21 @@ public class CategoryController {
 
     private CategoryService categoryService;
 
+    @Operation(summary = "Create a new category")
     @PostMapping
     public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO) {
         CategoryDTO savedCategory = categoryService.createCategory(categoryDTO);
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get all categories")
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> allCategories() {
         List<CategoryDTO> allCategories = categoryService.getAllCategories();
         return new ResponseEntity<>(allCategories, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a category by id")
     @GetMapping("/{id}")
     public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long id) {
         CategoryDTO categoryDTO = categoryService.getCategoryById(id);
--- src/main/java/com/ivanrudik/store/controller/ProductController.java
+++ src/main/java/com/ivanrudik/store/controller/ProductController.java
@@ -2,11 +2,14 @@ package com.ivanrudik.store.controller;
 
 import com.ivanrudik.store.dto.ProductDTO;
 import com.ivanrudik.store.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Product controller")
 @RestController
 @RequestMapping("/api/category/{categoryId}/products")
 @AllArgsConstructor
@@ -14,41 +17,33 @@ public class ProductController {
 
     private ProductService productService;
 
+    @Operation(summary = "Get products by id of category")
     @GetMapping
     public Page<ProductDTO> getProductsByCategoryId(
             @PathVariable Long categoryId,
-//            @RequestParam(defaultValue = "0") int page,
-//            @RequestParam(defaultValue = "10") int size,
-//            @RequestParam(defaultValue = "id") String sortField,
-//            @RequestParam(defaultValue = "asc") String sortDirection,
             Pageable pageable) {
         return productService.getProductsByCategoryId(categoryId, pageable);
     }
 
+    @Operation(summary = "Save a new product by id")
     @PostMapping
     public Page<ProductDTO> saveProductsByCategoryId(
             @PathVariable Long categoryId,
             @RequestBody ProductDTO productDTO,
-//            @RequestParam(defaultValue = "0") int page,
-//            @RequestParam(defaultValue = "10") int size,
-//            @RequestParam(defaultValue = "id") String sortField,
-//            @RequestParam(defaultValue = "asc") String sortDirection,
             Pageable pageable) {
         return productService.createProduct(categoryId, productDTO, pageable);
     }
 
+    @Operation(summary = "Update a product by id")
     @PutMapping
     public Page<ProductDTO> updateProductsByCategoryId(
             @PathVariable Long categoryId,
             @RequestBody ProductDTO productDTO,
-//            @RequestParam(defaultValue = "0") int page,
-//            @RequestParam(defaultValue = "10") int size,
-//            @RequestParam(defaultValue = "id") String sortField,
-//            @RequestParam(defaultValue = "asc") String sortDirection,
             Pageable pageable) {
         return productService.updateProduct(categoryId, productDTO, pageable);
     }
 
+    @Operation(summary = "Delete a product by id")
     @DeleteMapping("/{productId}")
     public Page<ProductDTO> deleteProductsByCategoryId(
             @PathVariable Long categoryId,
